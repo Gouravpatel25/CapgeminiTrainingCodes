369d959a9c0419fd944f8c869827f0bd279a3a9a
GOURAV <gouravpatel20032003@gmail.com>
2025-04-04 12:10:55 +0530

    First commit

--- src/Comparator/Book.java
+++ src/Comparator/Book.java
@@ -1,5 +1,24 @@
 package Comparator;
 
-public class Book {
+import java.util.Comparator;
 
+public class Book implements Comparator{
+	int id;
+	String name;
+	double price;
+	
+	public Book(int id, String name, double price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	@Override
+	public int compare(Object o1, Object o2) {
+	
+		return 0;
+	}
+	
+	
+	
 }
